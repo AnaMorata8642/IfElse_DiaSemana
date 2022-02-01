c27be6deaba05a4581e92891d387b301b6b5273c
AnaMorata <90793364+AnaMorata8642@users.noreply.github.com>
2022-02-01 11:36:32 -0300

    Add files via upload

---
+++ IfElse_DiaSemana/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace IfElse_DiaSemana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diaSemana = 0;
+
+            Console.WriteLine("Digite o número de um dia da semana:");
+            diaSemana = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            if (diaSemana == 1)
+            {
+                Console.WriteLine("O dia da semana correspondente é: Domingo");
+                Console.WriteLine("*****************************************");
+                Console.WriteLine("");
+            }
+
+            else if (diaSemana == 2)
+            {
+                Console.WriteLine("O dia da semana correspondente é: Segunda");
+                Console.WriteLine("*****************************************");
+                Console.WriteLine("");
+            }
+
+            else if (diaSemana == 3)
+            {
+                Console.WriteLine("O dia da semana correspondente é: Terça");
+                Console.WriteLine("*****************************************");
+                Console.WriteLine("");
+            }
+
+            else if (diaSemana == 4)
+            {
+                Console.WriteLine("O dia da semana correspondente é: Quarta");
+                Console.WriteLine("*****************************************");
+                Console.WriteLine("");
+            }
+
+            else if (diaSemana == 5)
+            {
+                Console.WriteLine("O dia da semana correspondente é: Quinta");
+                Console.WriteLine("*****************************************");
+                Console.WriteLine("");
+            }
+
+            else if (diaSemana == 6)
+            {
+                Console.WriteLine("O dia da semana correspondente é: Sexta");
+                Console.WriteLine("*****************************************");
+                Console.WriteLine("");
+            }
+
+            else
+            {
+                Console.WriteLine("O dia da semana correspondente é: Sábado");
+                Console.WriteLine("*****************************************");
+            }
+        }
+    }
+}
